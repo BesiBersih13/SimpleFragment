2058cf6aa80a4223b1c50909853a7bf2f084966a
BesiBersih13 <abdullahazzam98@mail.ugm.ac.id>
2019-02-15 14:41:01 +0700

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements SimpleFragment.OnFragmentInteractionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openClick(View view){
+        SimpleFragment simpleFragment = new SimpleFragment();
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container,simpleFragment).commit();
+    }
+
+    @Override
+    public void onRadioButtonChoice(int choice) {
+        Toast.makeText(this,"CHOICE "+choice,Toast.LENGTH_LONG).show();
+    }
+}
