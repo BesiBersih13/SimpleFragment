36cd2371892cf3eefdfcb291c145f9f3532d6ca0
Abdullah Azzam Azhar <abdullahazzam98@mail.ugm.ac.id>
2019-02-22 13:16:08 +0700

    s

--- app/src/main/java/com/example/myapplication/SimpleFragment.java
+++ app/src/main/java/com/example/myapplication/SimpleFragment.java
@@ -23,33 +23,21 @@ import android.widget.TextView;
 public class SimpleFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String CHOICE = "choice";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private int mRadioButtonChoice=2;
 
     private OnFragmentInteractionListener mListener;
 
-    public SimpleFragment() {
-        // Required empty public constructor
+    public SimpleFragment(){
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SimpleFragment.
-     */
     // TODO: Rename and change types and number of parameters
-    public static SimpleFragment newInstance(String param1, String param2) {
+    public static SimpleFragment newInstance(int choice) {
         SimpleFragment fragment = new SimpleFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putInt(CHOICE, choice);
         fragment.setArguments(args);
         return fragment;
     }
@@ -57,10 +45,6 @@ public class SimpleFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -70,6 +54,13 @@ public class SimpleFragment extends Fragment {
         final View rootView = inflater.inflate(R.layout.fragment_simple, container, false);
         final RadioGroup radioGroup=rootView.findViewById(R.id.radio_group);
         final TextView textView = rootView.findViewById(R.id.fragment_header);
+
+        if(getArguments().containsKey(CHOICE)){
+            mRadioButtonChoice = getArguments().getInt(CHOICE);
+            if(mRadioButtonChoice!=2){
+                radioGroup.check(radioGroup.getChildAt(mRadioButtonChoice).getId());
+            }
+        }
 
         radioGroup.setOnCheckedChangeListener(
                 new RadioGroup.OnCheckedChangeListener() {
